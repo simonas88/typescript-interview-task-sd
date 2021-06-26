b79004473b1297a479d26727688d51d42a3d8360
Simonas Drungilas <s.drungilas@gmail.com>
2021-06-26 19:37:09 +0300

    Fix linting issues in configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,55 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.ts',
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(jpg)$/i,
-                use: [
-                    'file-loader',
-                ],
-            },
+  entry: './src/index.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
         ],
-    },
-    resolve: {
-        alias: {
-            '~': path.resolve('./src'),
-        },
-        extensions: ['.js', '.ts', '.tsx'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-    },
-    plugins: [
-        new Dotenv({}),
-        new HtmlWebpackPlugin({
-            inject: true,
-            template: path.resolve('./public/index.html'),
-        }),
+      },
+      {
+        test: /\.(jpg)$/i,
+        use: [
+          'file-loader',
+        ],
+      },
     ],
-    devServer: {
-        historyApiFallback: true,
-        contentBase: './',
-        hot: true
-    }
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve('./src'),
+    },
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  plugins: [
+    new Dotenv({}),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve('./public/index.html'),
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    hot: true,
+  },
 };
