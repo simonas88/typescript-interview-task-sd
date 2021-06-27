707d578881ca7c507dd02f1c9201221ad3672de1
Simonas Drungilas <s.drungilas@gmail.com>
2021-06-27 18:10:30 +0300

    Encapsulate modal controls

--- src/components/PasswordHealth/components/List/List.tsx
+++ src/components/PasswordHealth/components/List/List.tsx
@@ -2,6 +2,7 @@ import React, { FC, useCallback, useState } from 'react';
 import { IItem } from '~/services/getUserItems';
 import ItemIcon from './components/ItemIcon';
 import Modal from 'react-modal';
+import useModal from './useModal';
 
 import './list-style.scss';
 
@@ -16,20 +17,20 @@ interface IUpdateModal {
 }
 
 const UpdateModal: FC<IUpdateModal> = ({ item, onUpdate }) => {
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, openModal, closeModal] = useModal(false);
   const [newPass, setNewPass] = useState('');
   
   const handleUpdate = useCallback(() => onUpdate({ ...item, password: newPass }), [item, newPass]);
 
   return (
     <>
-      <button className="update" onClick={() => setShowModal(true)}>
+      <button className="update" onClick={openModal}>
         Update Password
       </button>
       <Modal
         className="modal"
         isOpen={showModal}
-        onRequestClose={() => setShowModal(false)}
+        onRequestClose={closeModal}
         contentLabel="Example Modal"
       >
         <h1>Update Password</h1>
@@ -43,7 +44,7 @@ const UpdateModal: FC<IUpdateModal> = ({ item, onUpdate }) => {
           <button className="button" onClick={handleUpdate}>Change</button>
           <button className="button ml-12px" onClick={() => {
             setNewPass('');
-            setShowModal(false);
+            closeModal();
           }}>
             Cancel
           </button>
