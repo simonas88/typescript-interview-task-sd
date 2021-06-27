58f81815632a19df2c496ca0c85a1894822ea2a3
Simonas Drungilas <s.drungilas@gmail.com>
2021-06-27 20:08:07 +0300

    Clean up <Header /> component

--- src/components/PasswordHealth/components/Header/Header.tsx
+++ src/components/PasswordHealth/components/Header/Header.tsx
@@ -1,24 +1,25 @@
-import React, { FC, Fragment } from 'react';
+import React from 'react';
 
 import './header-style.scss';
+
 interface IHeader {
   vulnerableItems: number;
   username: string;
   onLogout: () => void;
 }
 
-const Header: FC<IHeader> = ({ vulnerableItems, username, onLogout }) => (
-  <div className={`header${vulnerableItems > 0 ? ' header--warning' : ''}`}>
+const Header: React.FC<IHeader> = ({ vulnerableItems, username, onLogout }) => (
+  <header className={`header${vulnerableItems > 0 ? ' header--warning' : ''}`}>
     <div className="user-section">
       <button onClick={onLogout}>{`Logout ${username}`}</button>
     </div>
     { vulnerableItems > 0 ? (
-      <Fragment>
+      <>
         <h1>{`${vulnerableItems} Items are vulnerable`}</h1>
         <span>Create new complex passwords to protect your accounts</span>
-      </Fragment>
+      </>
     ) : <h1>All items in order</h1> }
-  </div>
+  </header>
 );
 
 export default Header;
