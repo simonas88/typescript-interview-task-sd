47664b8b18f65d39faa315af42a3abbf0dfdd517
Simonas Drungilas <s.drungilas@gmail.com>
2021-06-27 22:20:37 +0300

    Make <PasswordForm /> a form

--- src/components/PasswordHealth/components/PasswordForm/PasswordForm.tsx
+++ src/components/PasswordHealth/components/PasswordForm/PasswordForm.tsx
@@ -1,4 +1,9 @@
 import React, { useCallback, useState } from 'react';
+import ErrorBlock from '~/components/ErrorBlock';
+import { IItem } from '~/services/getUserItems';
+import itemHasWeakPassword from '~/utils/itemHasWeakPassword';
+
+import './password-form-style.scss';
 
 type PasswordProps = {
   onConfirm: (password: string) => void;
@@ -7,6 +12,8 @@ type PasswordProps = {
 
 const PasswordForm: React.FC<PasswordProps> = ({ onConfirm, onCancel }) => {
   const [newPass, setNewPass] = useState('');
+
+  const passwordTooWeak = itemHasWeakPassword({ password: newPass } as IItem);
   
   const handleConfirm = useCallback(
     () => void onConfirm(newPass),
@@ -21,18 +28,28 @@ const PasswordForm: React.FC<PasswordProps> = ({ onConfirm, onCancel }) => {
   return (
     <>
       <h1>Update Password</h1>
-      <input
-        placeholder="new password"
-        className="input"
-        value={newPass}
-        onChange={(event) => setNewPass(event.target.value)} 
-      />
-      <div className="pt-12px text-center">
-        <button className="button" onClick={handleConfirm}>Change</button>
-        <button className="button ml-12px" onClick={handleCancel}>
-          Cancel
-        </button>
-      </div>
+      <form
+        className="password-form"
+        onSubmit={handleConfirm}>
+        <input
+          placeholder="new password"
+          className="input"
+          value={newPass}
+          onChange={(event) => setNewPass(event.target.value)} 
+        />
+        { newPass && passwordTooWeak ? <ErrorBlock error="Password too weak" /> : null }
+        <div className="pt-12px text-center">
+          <button
+            className="button"
+            type="submit"
+            disabled={passwordTooWeak}>
+            Change
+          </button>
+          <button className="button ml-12px" onClick={handleCancel}>
+            Cancel
+          </button>
+        </div>
+      </form>
     </>
   );
 };
